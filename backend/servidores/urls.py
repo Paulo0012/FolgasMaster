8eb4872cacc48d0be74ed43d3896dd03c73c140c
Paulo <paulo.gabriel1019@gmail.com>
2026-04-24 11:08:23 -0300

    feat: atualiando backend/servidores/urls.py, deixando apenas o necesario

--- backend/servidores/urls.py
+++ backend/servidores/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ServidorViewSet, AfastamentoViewSet, HoraExtraViewSet
+from .views import ServidorViewSet, AfastamentoViewSet
 
 router = DefaultRouter()
 router.register(r'servidores', ServidorViewSet)
 router.register(r'afastamentos', AfastamentoViewSet)
-router.register(r'horas-extras', HoraExtraViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
