0837f3fce08175964480282b3cb66dbedc7bb5a3
Paulo0012 <paulo.gabriel1019@gmail.com>
2026-04-24 16:06:55 -0300

    feat: atualizando backend/folgasprojeto/settings.py, atualizando ALlowed

--- backend/folgasprojeto/settings.py
+++ backend/folgasprojeto/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Use os dados do .env
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # ------------------------------------------------------------------
 # 1. APLICAÇÕES INSTALADAS
