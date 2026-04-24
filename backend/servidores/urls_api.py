c031619f4c184733c2d1c8feb093d9fe34fbdc6b
Paulo0012 <paulo.gabriel1019@gmail.com>
2026-04-24 15:45:13 -0300

    feat: atualizando backend/servidores/urls_api.py, adicioanndo nova rota

--- backend/servidores/urls_api.py
+++ backend/servidores/urls_api.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ServidorViewSet, AfastamentoViewSet
+from .views import ServidorViewSet, AfastamentoViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r'servidores', ServidorViewSet)
 router.register(r'afastamentos', AfastamentoViewSet)
-
+router.register(r'usuarios', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
