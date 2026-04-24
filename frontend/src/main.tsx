33278fa6afa3e8c67b669d439e5433d7dc59ea7b
Paulo0012 <paulo.gabriel1019@gmail.com>
2026-04-24 16:26:26 -0300

    feat: Atualizando frontend/main.tsx, atualizando

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-// Importações Globais de Estilo
+// Importações Locais (npm)
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'animate.css'
+
+// Seus estilos personalizados
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
